1ac13448c973c860d95e6e115582e3c74ebc62c1
dimashbalabek <dimashbalabek0@gmail.com>
2024-04-24 14:06:47 +0600

    added bonus

--- lib/bonus3.dart
+++ lib/bonus3.dart
@@ -1,3 +1,9 @@
 import 'dart:io';
 
-int nMonth = int.parse(stdin.readLineSync()!);
+void main(){
+  int nDay = int.parse(stdin.readLineSync()!);
+  var sdays = [0, 1, 2, 3, 4, 5, 6];
+  var current_day = (nDay-1) % 7;
+  print(current_day);
+
+}
