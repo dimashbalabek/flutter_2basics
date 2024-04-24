386a6efbf439791e4154a021be1eae98176f1657
dimashbalabek <dimashbalabek0@gmail.com>
2024-04-24 14:14:45 +0600

    add

--- lib/task4.dart
+++ lib/task4.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 import 'dart:math';
 
 /*------------------------- Task 4------------------------ */
- void  main(){
-   int a = int.parse(stdin.readLineSync()!);
-   double x = sqrt(((2*a) + sin(3*a)) / 3.56);
-   print(x);
- }
-
+void main() {
+  int a = int.parse(stdin.readLineSync()!);
+  double xValue = sqrt(((2 * a) + sin(3 * a).abs()) / 3.56);
+  print(xValue);
+}
